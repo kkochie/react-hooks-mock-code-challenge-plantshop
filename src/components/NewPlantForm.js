74f5c94f777d4d305dff6b95e00d43d8173d3ed8
Kristine Kochie <kristinekochie@gmail.com>
2022-07-07 13:10:22 -0400

    commit tree 264670879721a90f29a0de5b527f893bc3838783

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ addNewPlantForm }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: 0,
+  });
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
   return (
-    <div className="new-plant-form">
+    <div
+      className="new-plant-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        let newPlant = {
+          name: formData.name,
+          image: formData.image,
+          price: formData.price,
+        };
+        addNewPlantForm(newPlant);
+
+        setFormData({
+          name: "",
+          image: "",
+          price: 0,
+        });
+      }}
+    >
       <h2>New Plant</h2>
       <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Plant name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          value={formData.image}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          step="0.01"
+          placeholder="Price"
+          value={formData.price}
+          onChange={handleChange}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
