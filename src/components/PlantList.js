f25e2821744dc5bd5d679c3f82cfaf975b8cdbe2
Kristine Kochie <kristinekochie@gmail.com>
2022-07-07 13:11:22 -0400

    Core deliverables

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+function PlantList({ plantArray }) {
+  const cards = plantArray.map((eachPlant) => {
+    return <PlantCard key={`Plant-${eachPlant.id}`} plant={eachPlant} />;
+  });
+
+  return <ul className="cards">{cards}</ul>;
 }
 
 export default PlantList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [getPlants, setGetPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setGetPlants(data));
+  }, []);
+
+  function addNewPlant(newPlant) {
+    setGetPlants([...getPlants, newPlant]);
+
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newPlant),
+    });
+  }
+
+  const plantSearchArray = getPlants.filter((plant) => {
+    return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlantForm={addNewPlant} />
+      <Search searchTerm={searchTerm} onChangeSearch={setSearchTerm} />
+      <PlantList plantArray={plantSearchArray} />
     </main>
   );
 }
